30da3e4dd54354d9a4cae41705d88e46ba50bccc
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2025-03-22 22:49:14 +0200

    fix: commands

--- prepare.sh
+++ prepare.sh
@@ -10,13 +10,18 @@ fi
 
 if [[ ! -f "$LUA.tar.gz" ]]; then
 	wget "https://www.lua.org/ftp/$LUA.tar.gz"
-	tar -xvf "$LUA.tar.gz"
-	mv lua-5.4.7 lua
 fi
+
+rm -rf lua/
+tar -xvf "$LUA.tar.gz"
+mv lua-5.4.7 lua
 
 echo '/* blank */' > lua/src/locale.h
 cd lua/
-unix2dos src/*
+unix2dos src/* 
+unix2dos ../lua-5.4.7-for-dos-with-borland-turbo-c-2.01.patch
+
 # force using binary to avoid incositency with line-endinds, by some reason
 # unix2dos does not always helps
 patch -p1 -i "../lua-5.4.7-for-dos-with-borland-turbo-c-2.01.patch" --binary
+
